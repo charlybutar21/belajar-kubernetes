368d968492eb39cb2a852f0af6cbc545478f5d84
Charly Micolas Butarbutar <charlymicolasbutarbutar@pinjamwinwin.com>
2024-10-14 14:49:15 +0700

    add template files

--- kubectl-commands.sh
+++ kubectl-commands.sh
@@ -1,9 +1,43 @@
+#!/bin/bash
 ## list created pods
 kubectl get pod
 
-## submit resource to kubernetes cluser
+## submit resource to kubernetes cluster (create if not exist)
 kubectl create -f examples/nginx.yaml
-kubectl create -f examples/nginx-with-label.yml
+kubectl create -f examples/nginx-with-label.yaml
+kubectl create -f examples/nginx-wit-annotation.yaml
 
 ## list created pods with label information
-kubectl get pod --show-label
+kubectl get pods --show-labels
+
+## detail pod
+kubectl describe pod nginx-with-annotation
+
+## update label information
+kubectl label pod nginx environment=development
+kubectl label pod nginx environment=staging --overwrite
+
+## search pod
+kubectl get pods -l environment=staging
+
+## add annotation
+kubectl annotate pod nginx-with-annotation sesuatu-baru=Ini annotatation
+
+## view namespace
+kubectl get namespaces
+
+## view pod in specific namespace
+kubectl get pods --namespace default
+
+## create namespace
+kubectl create -f examples/finance-namespace.yaml
+
+## create pod in specific namespace
+kubectl create -f nginx.yaml --namespace finance-namespace
+
+## delete namespace
+kubectl delete namespace finance-namespace
+
+## delete pod
+kubectl delete pod nginx
+
