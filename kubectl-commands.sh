9b21726e223178fc3021c48e5df57d9490209ab8
Charly Micolas Butarbutar <charlymicolasbutarbutar@pinjamwinwin.com>
2024-10-18 11:29:00 +0700

    add examples

--- kubectl-commands.sh
+++ kubectl-commands.sh
@@ -2,10 +2,19 @@
 ## list created pods
 kubectl get pod
 
-## submit resource to kubernetes cluster (create if not exist)
+## submit resource to kubernetes cluster
 kubectl create -f examples/nginx.yaml
 kubectl create -f examples/nginx-with-label.yaml
 kubectl create -f examples/nginx-wit-annotation.yaml
+kubectl create -f examples/nginx-with-probe.yaml
+kubectl create -f examples/finance-namespace.yaml
+kubectl create -f examples/nginx-rc.yaml
+kubectl create -f examples/nginx-rs.yaml
+kubectl create -f examples/nginx-rs-match-expression.yaml
+kubectl create -f examples/daemon-nginx.yaml
+kubectl.exe create -f examples/job-nodejs.yaml
+kubectl.exe create -f examples/cronjob-nodejs.yaml
+
 
 ## list created pods with label information
 kubectl get pods --show-labels
@@ -41,3 +50,32 @@ kubectl delete namespace finance-namespace
 ## delete pod
 kubectl delete pod nginx
 
+## view replication controller
+kubectl get replicationcontrollers
+kubectl get replicationcontroller
+kubectl get rc
+
+## delete replication controller
+kubectl delete rc nginx-rc
+
+## delete replication controller without delete pod
+kubectl delete rc nginx-rc --cascade=false
+
+## view replica set
+kubectl get replicasets
+kubectl get replicaset
+kubectl get rs
+
+## view daemon set
+kubectl get daemonsets
+kubectl get daemonset
+kubectl get ds
+
+## view job
+kubectl get jobs
+kubectl get job
+
+## view cron jobs
+kubectl get cronjobs
+kubectl get cronjob
+kubectl get cj
